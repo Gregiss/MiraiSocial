19ddab55544817e359e1173152869d5af068e125
Alexandre Silva <xande1231231@hotmail.com>
2019-05-02 15:01:12 -0300

    Fix DB

--- config/config.php
+++ config/config.php
@@ -1,9 +1,6 @@
 <?php
 
-//Conexão com banco de dados
 
-$username = "kaway404";
-$password = "xande123";
 
 //Template funcao
 function template($dir, $file){
@@ -11,9 +8,22 @@ function template($dir, $file){
     $title = 'MiraiSocial';
     $keywords = 'Social Network, Rede Social, geek, Coders, Decoder';
     //Não edite daqui pra baixo :)
-    require_once 'themes/template/'.$dir. '/' . $file; // Incluir arquivo template
+    //Conexão com banco de dados
+    $username = "kaway404";
+    $password = "xande123";
+    try {
+      $conn = new PDO('mysql:host=localhost;dbname=miraisocial', $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      require_once 'themes/template/index.php';
+    } catch(PDOException $e) {
+      require_once 'themes/template/error/database.php'; // Carregar página de erro
+    }
+    require 'themes/template/'.$dir. '/' . $file; // Incluir arquivo template
 }
 
+//Conexão com banco de dados
+$username = "kaway404";
+$password = "xande123";
 try {
       $conn = new PDO('mysql:host=localhost;dbname=miraisocial', $username, $password);
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
