880fbc037d6eb9daad557d1620a6151a0ca65570
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-05-02 20:41:20 -0300

    Add files via upload

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,14 +7,14 @@ $(".centerbro").click(function(){
         login = 0;
         $(".cadastrar").html('<i class="fas fa-sign-in-alt"></i>');
 		$(".centerbro .desc").html("Login");
-		$(".centerbro .desc").css("left" ,"4px");
+		$(".centerbro .desc").css("left" ,"0px");
     } else{
         login = 1;
         $("#register").css("left", "-200%");
         $("#login").css("left", "50%");
         $(".cadastrar").html('<i class="fas fa-plus"></i>');
 		$(".centerbro .desc").html("Registrar");	
-		$(".centerbro .desc").css("left" ,"-5px");
+		$(".centerbro .desc").css("left" ,"-9px");
     }
 });
 
